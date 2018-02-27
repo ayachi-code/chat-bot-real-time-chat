36066ac6f2a4239125ec06bde73a94952a461056
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-27 07:55:54 +0100

    Als server word gehost dan krijg je een message in de console

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const app = express();
 
-const server = app.listen(8080);
+const server = app.listen(8080,() => {
+    console.log("Server word gehost op port 8080 ")
+});
