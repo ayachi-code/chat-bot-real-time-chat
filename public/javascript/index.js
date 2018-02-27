2126fad1259ea40648baa5f34bbc5d69741b765d
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-27 08:56:27 +0100

    als enter word geklikt dan komt er enter is geklikt geconsoled

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,7 @@
 let de_invoer = addEventListener("keyup",(event) => {
     event.preventDefault();
+    //Als enter word geklikt
+    if (event.keyCode === 13) {
+            console.log("Enter is geklikt")
+    }
 });
