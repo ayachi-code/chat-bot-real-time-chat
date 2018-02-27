32126942a4e167486fbaa2f05429ac85553bcc0e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-27 09:08:52 +0100

    de bestand word geladen en als het geladen is en gelukt is word de gelukt functie uigevoerd anderd de fout

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,8 @@
 let input_waarden;
 let bilal_assistent = new RiveScript();
 
+bilal_assistent.loadFile("/bot/bot.rive",gelukt_bilal_assistent,fout_bilal_assistent);
+
 
 let de_invoer = addEventListener("keyup",(event) => {
     event.preventDefault();
