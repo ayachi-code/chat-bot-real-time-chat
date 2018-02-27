e1fbd189805b754b4d70da109d8abb05ee51c6e2
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-27 09:10:08 +0100

    gelukt functie gemaakt

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,6 +4,12 @@ let bilal_assistent = new RiveScript();
 bilal_assistent.loadFile("/bot/bot.rive",gelukt_bilal_assistent,fout_bilal_assistent);
 
 
+function gelukt_bilal_assistent() {
+    console.log("Yay de assistent is succesvol geladen")
+}
+
+
+
 let de_invoer = addEventListener("keyup",(event) => {
     event.preventDefault();
     //Als enter word geklikt
