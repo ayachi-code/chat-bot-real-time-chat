713193497542a65bbbab54aa39fc72c939cf4429
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-27 08:56:56 +0100

    console.log gecomment

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,6 @@ let de_invoer = addEventListener("keyup",(event) => {
     event.preventDefault();
     //Als enter word geklikt
     if (event.keyCode === 13) {
-            console.log("Enter is geklikt")
+            //console.log("Enter is geklikt")
     }
 });
