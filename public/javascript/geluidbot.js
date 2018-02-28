9120bb80ac565da7fa304fc30cfddb93e0de960e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-28 10:43:31 +0100

    if waarben ik is false dan shiw

--- public/javascript/geluidbot.js
+++ public/javascript/geluidbot.js
@@ -1,20 +1,36 @@
 const geluid = new p5.Speech();
 let text_boven = document.getElementById("welkoma");
+let waar_ben_ik = false;
 
 
 let geluid_knop = document.getElementById("geluid-m").addEventListener('click',() => {
+    waar_ben_ik = true;
     text_boven.innerText = "Mr bilal,al je vragen beantwoorden over chat-bilal.nl 'GELUID MODE' ";
+    if (waar_ben_ik) {
     $(document).ready(() => {
         $("#de_invoer").hide();
     });    
-    
+    } 
+    if(waar_ben_ik) {
+        $(document).ready(() => {
+            $("#chat-mic").show();
+        });
+    }
 
 })
 
-
+//Normal mode
 let normaal_knop = document.getElementById("geluid-normal").addEventListener('click',() => {
+    waar_ben_ik = false;
     text_boven.innerText = "Mr bilal,al je vragen beantwoorden over chat-bilal.nl 'text mode' ";
+    if (!waar_ben_ik) {
     $(document).ready(() => {
         $("#de_invoer").show();
     });
+    } 
+    if(!waar_ben_ik) {
+        $(document).ready(() => {
+            $("#chat-mic").hide();
+        });
+    }
 });
