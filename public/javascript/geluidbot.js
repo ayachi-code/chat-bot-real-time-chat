e4ab66c1db7958e967b522b57169330c4f8deab5
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-28 09:17:38 +0100

    elek keer als je op geluid mode klikt komt er +1 en als > 1 dan word het 0

--- public/javascript/geluidbot.js
+++ public/javascript/geluidbot.js
@@ -3,5 +3,12 @@ let waar = 0;
 
 
 let geluid_knop = document.getElementById("geluid-m").addEventListener('click',() => {
+    waar += 1;
+    console.log(waar)
 
+
+
+    if (waar > 1) {
+        waar = 0;
+    }
 })
