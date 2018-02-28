f59f3dfb4959b32779eca5fa96ce1d90322144fd
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-28 09:34:45 +0100

    variable is gelijk aan de id welkoma

--- public/javascript/geluidbot.js
+++ public/javascript/geluidbot.js
@@ -1,9 +1,9 @@
 const geluid = new p5.Speech();
-let text_boven;
+let text_boven = document.getElementById("welkoma");
 
 
 let geluid_knop = document.getElementById("geluid-m").addEventListener('click',() => {
-
+    
 
 
 })
