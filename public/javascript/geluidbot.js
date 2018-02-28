3262d7584282144825361d87afac22ec1c575af4
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-28 09:26:36 +0100

    true false systeem toegepast

--- public/javascript/geluidbot.js
+++ public/javascript/geluidbot.js
@@ -1,18 +1,9 @@
 const geluid = new p5.Speech();
-let waar = 0;
+let waar = false;
 
 
 let geluid_knop = document.getElementById("geluid-m").addEventListener('click',() => {
-    waar += 1;
-    console.log(waar)
+    waar = true;
+    
 
-
-
-    if (waar > 1) {
-        waar = 0;
-    } else if(waar == 0) {
-        //Normal mode
-    } else if (waar == 1) {
-        //Geluid mode
-    }
 })
